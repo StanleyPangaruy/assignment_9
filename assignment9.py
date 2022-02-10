36b2172a8a883c0eeebc0fbbd79255c74446e2de
StanleyPangaruy <pangaruystanley@gmail.com>
2022-02-10 22:04:18 +0800

    added some contact informations and made some few adjustments on the positioning of the the texts. utilitze more fpdf methods. WIP

--- assignment9.py
+++ assignment9.py
@@ -7,13 +7,13 @@ with open('resume.json') as f:
 class PDF(FPDF):
     def header(self):
         # Logo
-        self.image('2x2p.jpg', 160, 8, 45)
+        self.image('2x2p.jpg', 160, 10, 45)
         # Line break
         self.ln(50)
 
 pdf = PDF('P', 'mm', 'Letter')
 pdf.add_page()
-
+pdf.set_xy(10,10)
 pdf.set_font('times', 'B', 16)
 pdf.set_text_color(0,0,0)
 pdf.cell(0, 5, 'PROFILE', ln=True)
@@ -22,12 +22,24 @@ pdf.cell(0, 5, 'PROFILE', ln=True)
 pdf.set_font('times', '', 12)
 pdf.set_text_color(0,0,0)
 
-for proffy in data["profile"]:
-    pdf.cell(0, 10, proffy['name'], ln=True)
-    pdf.cell(0, 0, proffy['label'], ln=True)
-    pdf.cell(0, 10, proffy['email'], ln=True)
-    pdf.cell(0, 0, proffy['phone'], ln=True)
-    pdf.cell(0, 10, proffy['birthday'], ln=True)
-    pdf.cell(0, 0, proffy['address'], ln=True)
-    pdf.cell(0, 10, proffy['summary'], ln=True)
+for details in data["resume"]:
+    pdf.cell(0, 10, details['name'], ln=True)
+    pdf.cell(0, 0, details['label'], ln=True)
+    pdf.cell(0, 10, details['email'], ln=True)
+    pdf.cell(0, 0, details['phone'], ln=True)
+    pdf.cell(0, 10, details['birthday'], ln=True)
+    pdf.cell(0, 0, details['address'], ln=True)
+    pdf.cell(0, 10, 'Networks: ', ln=True)
+    pdf.cell(0, 0, details['provider1'], ln=True)
+    pdf.cell(0, 10, details['username1'], ln=True)
+    pdf.cell(0, 0, details['url1'], ln=True)
+    pdf.set_xy(100,50)
+    pdf.cell(0, 10, details['provider2'], ln=True)
+    pdf.set_xy(100,60)
+    pdf.cell(0, 0, details['username2'], ln=True)
+    pdf.set_xy(100,60)
+    pdf.cell(0, 10, details['url2'], ln=True)
+    pdf.line(10, 70, 205, 70)
+
 pdf.output('pdf_test2.pdf')
+
